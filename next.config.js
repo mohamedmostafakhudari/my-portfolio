86ed82baa1cfddcb45379b2cd6f9bdc11b6ec3d7
mohamedmostafakhudari <https://github.com/mohamedmostafakhudari>
2023-10-09 07:54:34 +0300

    deploy on vercel prep

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   trailingSlash: true,
   images: {
     unoptimized: true,
@@ -12,7 +11,6 @@ const nextConfig = {
       },
     ],
   },
-   
 };
 
 module.exports = nextConfig;
