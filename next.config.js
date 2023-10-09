4f77a1fd38013f56f578c5eb680c958cc820d6d4
mohamedmostafakhudari <https://github.com/mohamedmostafakhudari>
2023-10-09 08:33:53 +0300

    edit

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  //This line is for temporary situation and would be removed later
+  output: "export",
   trailingSlash: true,
   images: {
     unoptimized: true,
