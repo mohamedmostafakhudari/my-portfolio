890b57115093d1e2675c57571579502537623715
mohamedmostafakhudari <https://github.com/mohamedmostafakhudari>
2023-10-10 21:43:36 +0300

    Update Footer Copy

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -5,61 +5,87 @@ import React from 'react'
 import { TypographyLarge } from '@/components/ui/typography';
 import { FaFacebookF } from "react-icons/fa";
 import { FaXTwitter, FaGithub, FaLinkedinIn } from "react-icons/fa6";
-
+import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 import Icon from '@/components/ui/Icon';
-const socials = [
-  {
-    label: "Github",
-    icon: FaGithub,
-    href: "#",
-  },
-  {
-    label: "Linkedin",
-    icon: FaLinkedinIn,
-    href: "#",
-  },
-  {
-    label: "Twitter",
-    icon: FaXTwitter,
-    href: "#",
-  },
-  {
-    label: "Facebook",
-    icon: FaFacebookF,
-    href: "#",
-  },
-];
+const socials = {
+  professional: [
+    {
+      label: "Github",
+      icon: FaGithub,
+      href: "#",
+    },
+    {
+      label: "Linkedin",
+      icon: FaLinkedinIn,
+      href: "#",
+    },
+  ],
+  social: [
+    {
+      label: "Twitter",
+      icon: FaXTwitter,
+      href: "#",
+    },
+    {
+      label: "Facebook",
+      icon: FaFacebookF,
+      href: "#",
+    }
+  ],
+};
 
 const Footer = () => {
   return (
-    <footer className="pt-16 bg-gradient-to-b from-white/5 to-transparent">
+    <footer className="bg-gradient-to-b from-white/5 to-transparent pt-16">
       <div className="container max-w-screen-lg">
         <div className="flex flex-col gap-12 sm:flex-row sm:flex-wrap sm:justify-between">
-          <div className='space-y-8 max-w-[42ch]'>
-            <TypographyLarge>About Me</TypographyLarge>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Accusantium, dolorem.
+          <div className="max-w-[42ch] space-y-8">
+            <TypographyLarge>Lets Work Together</TypographyLarge>
+            <div className="space-y-4">
+              <p>
+                I would be delighted to be part of your online success and help
+                your business to reach new heights.
+              </p>
+              <Button asChild>
+                <Link href="./contact">I&apos;m In</Link>
+              </Button>
+            </div>
+            <p className="hidden md:block">
+              Copyright &copy; 2023 All rights reserved
             </p>
-            <p>Copyright &copy;2023 All rights reserved</p>
           </div>
-          <div className="space-y-8">
-            <TypographyLarge>Let Us Be Friends</TypographyLarge>
-            <div className="flex gap-2">
-              {socials.map(({ label, icon, href }) => (
-                <Icon
-                  key={label}
-                  IconComp={icon}
-                  href={href}
-                />
-              ))}
+          <div>
+            <div className="space-y-8">
+              <TypographyLarge>My Professional Profiles</TypographyLarge>
+              <div className="flex gap-2">
+                {socials.professional.map(({ label, icon, href }) => (
+                  <Icon key={label} IconComp={icon} href={href} />
+                ))}
+              </div>
+              <div className="space-y-8">
+                <TypographyLarge>Let Us Be Friends</TypographyLarge>
+                <div className="flex gap-2">
+                  {socials.social.map(({ label, icon, href }) => (
+                    <Icon key={label} IconComp={icon} href={href} />
+                  ))}
+                </div>
+              </div>
             </div>
           </div>
         </div>
+        <p className="mt-12 block md:hidden">
+          Copyright &copy; 2023 All rights reserved
+        </p>
       </div>
-      <div className="text-center py-4 bg-white/5 mt-12">
-          Made with &lt;3 by <Link href="#" className="text-sky-600 duration-200 ease-in-out hover:text-sky-500">Mohamed Mostafa</Link>
+      <div className="mt-12 bg-white/5 py-4 text-center">
+        Made with &lt;3 by{" "}
+        <Link
+          href="#"
+          className="text-sky-600 duration-200 ease-in-out hover:text-sky-500"
+        >
+          Mohamed Mostafa
+        </Link>
       </div>
     </footer>
   );
